f8d58956b100221ae3372bb289ae1751ce71a7ff
8go <cccc@tuta.io>
2020-10-28 11:07:20 +0000

    linted .py file

--- eno/scripts/platforminfo.py
+++ eno/scripts/platforminfo.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
-
+"""Print distribution information to stdout."""
 import platform
 import sys
-import os
+
 
 def linux_distribution():
+    """Get distribution information."""
     try:
         return platform.linux_distribution()
     except BaseException:
         return "N/A"
 
 
-print("""Python version: %s
+print(
+    """Python version: %s
 linux_distribution: %s
 system: %s
 machine: %s
 platform: %s
 uname: %s
 version: %s
-""" % (
-    sys.version.split('\n'),
-    linux_distribution(),
-    platform.system(),
-    platform.machine(),
-    platform.platform(),
-    platform.uname(),
-    platform.version(),
-))
+"""
+    % (
+        sys.version.split("\n"),
+        linux_distribution(),
+        platform.system(),
+        platform.machine(),
+        platform.platform(),
+        platform.uname(),
+        platform.version(),
+    )
+)
 
 # EOF
