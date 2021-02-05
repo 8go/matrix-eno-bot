31095edec405cf198ff9c5e7f2475bb9bf0c9c3a
8go <cccc@tuta.io>
2021-02-05 07:43:02 +0000

    moved to TORPROXY instead of TORIFY

--- eno/scripts/eth.sh
+++ eno/scripts/eth.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-TORIFY="torify"
+# use TORIFY or TORPROXY, but do NOT use BOTH of them!
+# TORIFY="torify"
+TORIFY="" # replaced with TORPROXY, dont use both
+TORPROXY=" --socks5-hostname localhost:9050 "
 
 # tor and torify should be installed for your privacy.
 type torify >/dev/null 2>&1 || {
-	echo "It is recommended that you install the packge \"tor\" on the server for privacy."
-	TORIFY=""
+    echo "It is recommended that you install the packge \"tor\" on the server for privacy."
+    TORIFY=""
+    TORPROXY=""
 }
 
 # $TORIFY curl --silent --compressed "https://data.messari.io/api/v1/assets/ethereum/metrics" | jq '.data.market_data'   2>> /dev/null
-# $TORIFY curl --silent --compressed "https://data.messari.io/api/v1/assets/ethereum/metrics" | 
-# 	jq '.data.market_data | keys_unsorted[] as $k | "\($k), \(.[$k] )"' | 
+# $TORIFY curl --silent --compressed "https://data.messari.io/api/v1/assets/ethereum/metrics" |
+# 	jq '.data.market_data | keys_unsorted[] as $k | "\($k), \(.[$k] )"' |
 #	grep -e price_usd -e real_volume_last_24_hours -e percent_change_usd_last_24_hours -e price_btc | tr -d "\""  | rev | cut -c9- | rev  | tr "," ":" 2>&1
 
-BASELIST=$($TORIFY curl --silent --compressed "https://data.messari.io/api/v1/assets/ethereum/metrics" | 
-	jq '.data.market_data | keys_unsorted[] as $k | "\($k), \(.[$k] )"' |
-	grep -e price_usd -e real_volume_last_24_hours -e percent_change_usd_last_24_hours -e price_btc | tr -d "\"")
+BASELIST=$($TORIFY curl $TORPROXY --silent --compressed "https://data.messari.io/api/v1/assets/ethereum/metrics" |
+    jq '.data.market_data | keys_unsorted[] as $k | "\($k), \(.[$k] )"' |
+    grep -e price_usd -e real_volume_last_24_hours -e percent_change_usd_last_24_hours -e price_btc | tr -d "\"")
 # returns something like
 #       price_usd, 9632.330680167783
 #       real_volume_last_24_hours, 1418555108.5501404
