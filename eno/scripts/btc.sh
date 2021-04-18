e95f81ddbbf7b3a36c9871a7430425fc51166e15
8go <cccc@tuta.io>
2021-04-18 08:51:12 +0000

    added 2 new price lines

--- eno/scripts/btc.sh
+++ eno/scripts/btc.sh
@@ -2,35 +2,35 @@
 
 # use TORIFY or TORPROXY, but do NOT use BOTH of them!
 # TORIFY="torify"
-TORIFY="" # replaced with TORPROXY, dont use both
+TORIFY="" # replaced with TORPROXY, don't use both
 TORPROXY=" --socks5-hostname localhost:9050 "
 
 # tor and torify should be installed for your privacy.
-type torify >/dev/null 2>&1 || {
-    echo "It is recommended that you install the packge \"tor\" on the server for privacy."
-    TORIFY=""
-    TORPROXY=""
+type torify > /dev/null 2>&1 || {
+  echo "It is recommended that you install the packge \"tor\" on the server for privacy."
+  TORIFY=""
+  TORPROXY=""
 }
 
 if [ "$DEBUG" == "1" ] || [ "${DEBUG,,}" == "true" ]; then
-    DEBUG="true"
+  DEBUG="true"
 fi
 if [ $# -gt 1 ]; then
-    echo "Too many arguments."
-    exit 1
+  echo "Too many arguments."
+  exit 1
 fi
 if [ "$#" == "1" ]; then
-    case "${1,,}" in
-    'notor' | 'notorify' | '--notor' | '--notorify')
-        [ "${DEBUG}" == "true" ] && echo "Torify and Tor Proxy will be turned off."
-        TORIFY=""
-        TORPROXY=""
-        ;;
-    *)
-        echo "Unknown argument $*."
-        exit 1
-        ;;
-    esac
+  case "${1,,}" in
+  'notor' | 'notorify' | '--notor' | '--notorify')
+    [ "${DEBUG}" == "true" ] && echo "Torify and Tor Proxy will be turned off."
+    TORIFY=""
+    TORPROXY=""
+    ;;
+  *)
+    echo "Unknown argument $*."
+    exit 1
+    ;;
+  esac
 fi
 
 # # OPTION 1: MESSARI SOURCE
@@ -65,13 +65,13 @@ fi
 #   "bpi":{"USD":{"code":"USD","symbol":"&#36;","rate":"48,011.8840","description":"United States Dollar","rate_float":48011.884},...,
 #          "EUR":{"code":"EUR","symbol":"&euro;","rate":"39,745.6779","description":"Euro","rate_float":39745.6779}}}
 BASELIST2=$($TORIFY curl $TORPROXY --silent --compressed "https://api.coindesk.com/v1/bpi/currentprice.json" |
-    jq '.bpi.EUR.rate_float, .bpi.USD.rate_float')
+  jq '.bpi.EUR.rate_float, .bpi.USD.rate_float')
 if [ "$BASELIST2" == "" ]; then
-    echo "Error: No data available. Are you sure the network is up?"
-    if [ "${TORIFY}" != "" ] || [ "${TORPROXY}" != "" ]; then
-        echo "Error: Are you sure Tor is running? Start Tor or add 'notor' as argument!"
-    fi
-    exit 1
+  echo "Error: No data available. Are you sure the network is up?"
+  if [ "${TORIFY}" != "" ] || [ "${TORPROXY}" != "" ]; then
+    echo "Error: Are you sure Tor is running? Start Tor or add 'notor' as argument!"
+  fi
+  exit 1
 fi
 
 LC_ALL=en_US.UTF-8 dm1=$(date +%F --date='yesterday')
@@ -82,8 +82,9 @@ LC_ALL=en_US.UTF-8 dm3=$(date +%F --date='3 days ago')
 # shellcheck disable=SC2089
 jqargs=".bpi.\"${dm1}\",.bpi.\"${dm2}\",.bpi.\"${dm3}\""
 BASELIST3=$($TORIFY curl $TORPROXY --silent --compressed "https://api.coindesk.com/v1/bpi/historical/close.json?start=${dm3}&end=${dm1}" |
-    jq "$jqargs")
+  jq "$jqargs")
 price_dm0="$(echo "$BASELIST2" | tail -n 1)"                              # price USD today
+price_em0="$(echo "$BASELIST2" | head -n 1)"                              # price EUR today
 price_dm1="$(echo "$BASELIST3" | head -n 1)"                              # price USD yesterday
 price_dm2="$(echo "$BASELIST3" | head -n 2 | tail -n 1)"                  # price USD 2 days ago
 price_dm3="$(echo "$BASELIST3" | tail -n 1)"                              # price USD 3 days ago
@@ -98,8 +99,11 @@ euro_emoji=💶
 [[ $change_dm2 == -* ]] && chart2=$chartdown_emoji || chart2=$chartup_emoji
 [[ $change_dm3 == -* ]] && chart3=$chartdown_emoji || chart3=$chartup_emoji
 # price, and trend in comparison to yesterday close
-LC_ALL=en_US.UTF-8 printf "Price: %'.0f EUR € %s %s\n" "$(echo "$BASELIST2" | head -n 1)" "$euro_emoji" "$chart1"
-LC_ALL=en_US.UTF-8 printf "Price: %'.0f USD $ %s %s\n" "$(echo "$BASELIST2" | tail -n 1)" "$dollar_emoji" "$chart1"
+LC_ALL=en_US.UTF-8 printf "Price: %'.0f EUR € %s %s\n" "$price_em0" "$euro_emoji" "$chart1"
+LC_ALL=en_US.UTF-8 printf "Price: %'.0f USD \$ %s %s\n" "$price_dm0" "$dollar_emoji" "$chart1"
+# today's price in satoshis per dollar (euro), today $1 gets you so many satoshis
+LC_ALL=en_US.UTF-8 printf "Price: €1 gets you %'.0f sats %s\n" "$(echo "scale=4; 100000000/$price_em0" | bc)" "$euro_emoji"
+LC_ALL=en_US.UTF-8 printf "Price: \$1 gets you %'.0f sats %s\n" "$(echo "scale=4; 100000000/$price_dm0" | bc)" "$dollar_emoji"
 # todays price compared to the price from last days, trend, if positiv then today's priceis higher, i.e. the price has gone up.
 LC_ALL=en_US.UTF-8 printf "Price: %'.0f USD (yesterday)   %+'5.1f %% %s \n" "$(echo "$BASELIST3" | head -n 1)" "$change_dm1" "$chart1"
 LC_ALL=en_US.UTF-8 printf "Price: %'.0f USD (2 days ago)  %+'5.1f %% %s \n" "$(echo "$BASELIST3" | head -n 2 | tail -n 1)" "$change_dm2" "$chart2"
