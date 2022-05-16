343bc768346a8a9a9d08e4113062110035e4af17
8go <cccc@tuta.io>
2022-05-16 11:24:09 +0000

    affirmations in French
    Helpful to create a daily positive affirmations.
    Like "message of the day".
    Note, this is in French.

---
+++ eno/scripts/affirmations.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+if [[ "$ENO_SENDER" != "" ]]; then
+   PREFIX="${ENO_SENDER}, "
+else
+   PREFIX=""
+fi
+
+MSG=$(echo "Le bonheur est mon droit imprescriptible. J’accepte que le bonheur devienne le cœur de mon existence.
+Je ressens de la joie et de la satisfaction en ce moment précis.
+Je me réveille chaque matin en me sentant heureux et enthousiaste à l’égard de ma vie.
+Je peux exploiter ma source de bonheur intérieur chaque fois que je le souhaite.
+En me permettant d’être heureux, j’incite les autres à être heureux aussi.
+Je m’amuse de tous mes efforts, même les plus banals.
+Je regarde le monde autour de moi et je ne peux qu’empêcher de sourire et de ressentir de la joie.
+Je trouve de la joie et du plaisir dans les choses les plus simples de la vie.
+J’ai le sens de l’humour et j’aime partager des rires avec les autres.
+Mon cœur déborde de joie.
+Mon partenaire et moi partageons un amour profond et puissant l’un pour l’autre.
+Je respecte et j’admire mon partenaire et je vois le meilleur en lui.
+J’aime mon partenaire exactement comment il est et j’apprécie ses qualités uniques.
+Mon partenaire et moi partageons de l’intimité émotionnelle chaque jour grâce à la conversation et au toucher.
+J’ai des limites saines avec mon partenaire.
+Mon partenaire et moi nous amusons ensemble et trouvons de nouvelles façons de profiter de notre temps ensemble.
+Mon partenaire et moi communiquons ouvertement et résolvons les conflits de manière pacifique et respectueuse.
+Je peux être entièrement moi-même et complètement authentique dans ma relation amoureuse.
+Je communique mes désirs et mes besoins de façon claire et en toute confiance avec mon partenaire.
+Je souhaite le meilleur à mon partenaire et je le soutiens dans chacune de ses entreprises.
+Je m’attends à réussir dans toutes mes entreprises, car le succès est mon état naturel.
+Je trouve facilement des solutions aux défis et aux obstacles et je les dépasse rapidement.
+Les erreurs et les contretemps sont des tremplins vers le succès, car j’apprends d’eux.
+Chaque jour, j’ai de plus en plus de succès.
+Je réussis ma vie maintenant, même si je travaille vers les succès futurs.
+Je sais exactement ce que je dois faire pour réussir.
+Je vois la peur comme un combustible pour ma réussite et je prends des risques malgré la peur.
+Je me sens puissant, capable, confiant, empli d’énergie et aux anges.
+J’ai l’intention de réussir et je sais que c’est une réalité qui m’attend à l’arrivée.
+J’ai maintenant atteint mon objectif _______ et je suis fier de ma réussite.
+Aujourd’hui, je réussis. Demain, je réussirai. Chaque jour, je réussis.
+Quand je respire, j’inspire la confiance et j’expire la timidité.
+J’adore rencontrer des étrangers et les aborder avec enthousiasme.
+Je vis au présent et je suis confiant dans l’avenir.
+Je fais preuve de confiance. Je suis audacieux et sociable.
+Je suis autonome, créatif et persévérant dans tout ce que je fais.
+Je suis énergique et enthousiaste. La confiance est ma seconde nature.
+Je n’attire que les meilleures opportunités et les personnes positives dans ma vie.
+Je suis un spécialiste en résolution de problème. Je me concentre sur les solutions et je trouve toujours la meilleure.
+J’aime le changement et je m’adapte facilement aux nouvelles situations.
+Je suis soigné, sain et confiant. Mon apparence est assortie à mon bien-être intérieur.
+Je développe ma confiance en moi. Rien n’est impossible et la vie est géniale.
+Je ne vois toujours que le bien chez les autres. J’attire uniquement des personnes confiantes et positives.
+Je m’accepte tel que je suis et je m’aime profondément et complètement.
+Je suis unique. Je me sens bien en étant vivant et en étant moi-même.
+Je me fais confiance et je sais que ma sagesse intérieure est mon meilleur guide.
+Je suis intègre. Je suis totalement fiable. Je fais ce que je dis.
+J’agis en toute sécurité.
+Je m’accepte pleinement et je sais que je suis digne des meilleures choses dans la vie.
+Je choisis d’être fier de moi-même.
+J’éprouve une profonde paix intérieure.
+Je me remplis l’esprit de pensées positives et nourrissantes.
+Confiance, estime de soi et sagesse intérieure croissent chaque jour.
+Mon système immunitaire est très fort et peut éliminer n’importe quel type de bactéries, de germes et de virus.
+Chaque cellule de mon corps vibre d’énergie et de santé.
+Je ne souffre pas d’aucune douleur, et mon corps est rempli d’énergie.
+Je nourris mon corps avec des aliments sains.
+Tous les organes de mon corps fonctionnent parfaitement.
+Mon corps guérit, et je me sens de mieux en mieux chaque jour.
+J’aime faire de l’exercice et renforcer mes muscles.
+À chaque respiration, je libère mon corps du stress.
+J’envoie de l’amour à tous les organes de mon corps.
+Je respire profondément, je fais régulièrement de l’exercice et je ne me nourris que d’aliments nutritifs pour mon corps.
+Je fais attention à ce dont mon corps a besoin pour la santé et la vitalité.
+Je dors profondément et paisiblement, et je me réveille reposé et plein d’énergie.
+Je suis entouré de personnes qui encouragent et soutiennent les choix sains.
+Mon univers est un endroit paisible, aimant et plein de joie.
+Je sème les germes de la paix partout où je vais.
+Je m’entoure de gens pacifiques.
+Mon milieu de travail est calme et paisible.
+J’inspire la paix, j’expire le chaos et le désordre.
+Ma maison est un sanctuaire paisible où je me sens en sécurité et heureux.
+Dans tout ce que je dis et fais, je choisis la paix.
+Je me libère des colères et des blessures passées et je me remplis de sérénité et de pensées pacifiques.
+La paix m’envahit maintenant et pour toujours.
+J’envoie de la paix dans le monde.
+Je réponds pacifiquement dans toutes les situations.
+Je m’appuie sur le moment présent.
+Je me concentre sur la tâche à accomplir.
+Tout va bien maintenant.
+Je suis reconnaissant pour ce moment et j’éprouve de la joie.
+Je reviens doucement et facilement au moment présent.
+J’observe mes pensées et mes actions sans les juger.
+Je suis pleinement présent dans toutes mes relations.
+La vie se passe en ce moment.
+J’accepte et j’accueille toutes les expériences, même désagréables.
+J’observe mes émotions sans m’y attacher.
+Je médite facilement sans résistance ni anxiété.
+Je me libère du passé et je vis pleinement le moment présent.
+La tranquillité m’envahit à chaque souffle profond que je prends.
+Chaque jour, je suis de plus en plus détendu.
+Être calme et détendu dynamise tout mon être.
+Tous les muscles de mon corps se relâchent et se relaxent.
+Toute la négativité et le stress s’évaporent de mon corps et de mon esprit.
+J’inspire la détente. J’expire le stress.
+Même quand il y a du chaos autour de moi, je reste calme et centré.
+Je dépasse le stress. Je vis en paix.
+Je suis dépourvu d’anxiété, et une profonde paix intérieure remplit mon esprit et mon corps.
+Tout va bien dans mon univers. Je suis calme., heureux et content.
+Je m’endors dans le bonheur, car je sais que tout va bien dans mon univers.
+« Je suis capable de tout »
+« Je peux le rêver donc je peux le faire »
+« J’ai en moi toutes les capacités pour réaliser mes projets et aller au bout de mes rêves »
+« Je suis douée, intelligente et capable de tout malgré ce que j’ai pu entendre et pensé »
+« J’ai décidé de m’aimer car c’est ma plus belle histoire d’amour et elle durera toute ma vie »
+« Aujourd’hui est le plus beau jour de ma vie car c’est le jour que je vis »
+« Je laisse le passé derrière moi et je sais qu’aujourd’hui sera une merveilleuse journée »
+« Les gens les plus heureux n’ont pas tout ce qu’il y a de mieux. Ils font juste de leur mieux avec tout ce qu’ils sont »
+« Chaque jour, de tous les points de vue, je vais de mieux en mieux »
+« Aujourd’hui, même s’il pleut, j’apprendrai à danser sous la pluie »
+« Parfois c’est différent de ce que tu espérais… et c’est mieux comme ça ! »
+« Quoi que tu fasses, fais ce qui te rend heureuse »
+« Chaque jour j’ai un peu plus de joie et de succès dans mon travail et dans ma vie »
+« Il n’est pas trop tard pour être ce que je veux être, mais c’est aujourd’hui que je dois m’y mettre »
+« J’accepte ce qui est, je laisse aller ce qui était et j’ai confiance en ce qui sera »
+« L’échec est simplement une façon de recommencer de manière plus intelligente »
+« Je n’ai pas besoin d’être parfait.e car la perfection n’existe pas. J’ai simplement besoin d’être et d’avancer »
+« Tant que je rêve, j’ose et je travaille, je suis capable d’y arriver »
+« Aujourd’hui est le seul jour pour réaliser mes rêves »
+« Je m’attends au meilleur pour lui donner toutes les chances d’apparaitre »
+J’habite un monde d’amour et d’acceptation.
+La vie m’aime et je suis en sécurité.
+Je parle avec sagesse et discernement.
+Je me réjouis de l’amour que j’ai à donner.
+C’est l’amour qui anime ma vie.
+Je reçois l’amour que je donne.
+Financièrement, je suis toujours à l’aise.
+L’amour inconditionnel c’est simplement un amour qui n’attend rien en retour.
+Je suis le créateur et l’acteur de ma vie.
+Les êtres sont comme des fleurs. Chacun à sa beauté propre, chacun s’ouvre et s’épanouit à sa manière et à son rythme.
+Toutes mes nouvelles habitudes m’aident de façon positive.
+Aujourd’hui je choisis de dépasser mes limites d’hier. Je suis prêt à m’ouvrir à quelque chose de nouveau.
+Je mets de l’amour dans mon regard et je vois tout clairement.
+Plus je comprends de choses et plus mon univers s’élargit.
+Je me fais toujours confiance.
+La manière dont nous voyons ce qu’il y a à l’extérieur de nous reflète ce qu’il y a en nous.
+Mon corps est un ami et j’en prends soin.
+Je me félicite pour les grandes et petites choses que je réalise.
+Je revendique mon pouvoir et je dépasse toutes mes limites.
+J’aime ce que je pense.
+Je donne à la Vie avec joie et la vie me le donne avec Amour.
+L’amour fait toujours disparaître la douleur.
+Les émotions sont des pensées qui sont actives dans notre corps.
+J’adore les enfants et les enfants m’adorent.
+M’aimer moi-même et les autres me permet de m’épanouir et de vivre au maximum de mes possibilités.
+Je m’aime dans toutes les expériences que je traverse et tout va bien.
+Je partage mes ressources et mon savoir avec la Vie.
+Je suis financièrement à l’aise.
+Mon corps est en paix, heureux, en bonne santé et moi aussi.
+En élargissant mes horizons, je fais facilement disparaître mes limites.
+Je suis une expression individuelle de la Vie.
+Je transforme les leçons à apprendre en partie de plaisir.
+Je fais des choix nouveaux, différents, plus positifs, qui me nourissent de l’intérieur.
+Ma maison et mon coeur sont des lieux de paix et de bonté.
+La seule chose que vous pouvez vraiment maîtriser c’est ce que vous pensez au moment présent.
+Vous avez tout pouvoir sur votre pensée du moment.
+Je me crée avec amour une santé parfaite.
+La sagesse que je cherche se trouve en moi.
+Chaque instant est un nouveau départ.
+Tout ce dont j’ai besoin est à portée de ma main.
+Je choisis un mode de vie paisible.
+Le pardon possède un pouvoir de guérison que j’ai toujours à ma disposition.
+Tout va bien. J’ai tout ce dont j’ai besoin en ce moment.
+Mes pensées déterminent ma vie.
+Je découvre maintenant de nouvelles et merveilleuses expériences. Je suis en sécurité.
+Je concentre doucement mon esprit sur les belles choses de la vie.
+Le pouvoir se trouve dans le moment présent. Affirmez votre pouvoir.
+Ma conscience est riche.
+Je me libère et je pardonne.
+C’est avec moi-même que j’entretiens la meilleure relation.
+J’ai la responsabilité de ma vie." | shuf -n1)
+
+echo "${PREFIX}$MSG"
+
+# EOF
